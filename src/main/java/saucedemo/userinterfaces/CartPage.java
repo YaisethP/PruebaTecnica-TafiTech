bf640aa9ecb91211d8c076975f4e8104a29fdcd8
YaisethP <58501883+YaisethP@users.noreply.github.com>
2025-10-24 14:53:37 -0500

    Delete src directory

--- src/main/java/saucedemo/userinterfaces/CartPage.java
+++
@@ -1,9 +0,0 @@
-package saucedemo.userinterfaces;
-
-import net.serenitybdd.screenplay.targets.Target;
-import org.openqa.selenium.By;
-
-public class CartPage {
-    public static final Target PRODUCT_NAME = Target.the("nombre del producto en el carrito")
-            .located(By.cssSelector(".inventory_item_name"));
-}
